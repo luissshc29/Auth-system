171f8880bf2bc8332086d84ca84a34d530726257
Luis Henrique <luishcc2003@gmail.com>
2023-12-16 23:32:21 -0300

    atualizando tela de cliente (gif e disposição das informações), e adicionando mensagens de erro nos formulários

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -2,36 +2,53 @@
 
 import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { LuLogOut } from "react-icons/lu";
 
 export default function Client() {
     const session = useSession();
 
     if (session.status === "loading") {
-        return <h1 className="p-8 text-xl font-bold">Please wait ...</h1>;
+        return <h1 className="p-8 text-2xl font-extrabold">Please wait ...</h1>;
     }
 
     return (
-        <div className="p-8">
-            <div className="flex w-full justify-between">
-                <h1 className="text-2xl font-extrabold mb-4">
-                    Welcome, {session.data?.user?.name}!
-                </h1>
+        <div className="p-8 text-center">
+            <div className="flex w-full justify-end">
                 {session.data && (
                     <Link
                         href="/api/auth/signout"
-                        className="underline font-bold text-red-600 text-lg"
+                        className="rounded-[50%] p-2 bg-red-600 text-white scale-110 hover:scale-125 duration-300"
                     >
-                        Logout
+                        <LuLogOut />
                     </Link>
                 )}
             </div>
-            {session.data?.user?.image && (
-                <img
-                    src={session.data?.user?.image as string}
-                    alt="Avatar"
-                    className="w-28 h-28 rounded-full mb-4"
-                />
-            )}
+            <div className="flex flex-col place-items-center rounded-2xl px-8 py-12 w-full mx-auto items-center shadow-xl shadow-neutral-300">
+                <div className="flex flex-col-reverse md:flex-row items-center gap-4">
+                    <h1 className="text-xl md:text-2xl font-extrabold mb-4">
+                        Welcome, {session.data?.user?.name}!
+                    </h1>
+                    <img
+                        src={
+                            session.data?.user?.image ||
+                            "https://media.istockphoto.com/id/1337144146/pt/vetorial/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=_XeYoSJQIN7GrE08cUQDJCo3U7yvoEp5OKpbhQzpmC0="
+                        }
+                        alt="Avatar"
+                        className="w-12 h-12 md:w-16 md:h-16 rounded-full mb-4"
+                    />
+                </div>
+
+                <div>
+                    <img
+                        src="https://cdn.dribbble.com/users/2185205/screenshots/7886140/media/90211520c82920dcaf6aea7604aeb029.gif"
+                        alt="Successfully authenticated gif"
+                    />
+                    <h2 className="text-lg md:text-xl font-medium text-green-600">
+                        You were successfully{" "}
+                        <span className="font-extrabold">authenticated</span>!
+                    </h2>
+                </div>
+            </div>
         </div>
     );
 }
--- src/app/components/SigninForm/SigninForm.tsx
+++ src/app/components/SigninForm/SigninForm.tsx
@@ -3,10 +3,16 @@
 import { InputData } from "@/utils/types/InputData";
 import { BuiltInProviderType } from "next-auth/providers/index";
 import { LiteralUnion, signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { FaGithub } from "react-icons/fa";
 
 export default function SigninForm() {
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string>("");
+    const router = useRouter();
+
     const {
         register,
         handleSubmit,
@@ -16,18 +22,31 @@ export default function SigninForm() {
 
     const onSubmit: SubmitHandler<InputData> = async (data) => {
         try {
-            signIn("credentials", {
+            setLoading(true);
+            const res = await signIn("credentials", {
                 username: data.username,
                 password: data.password,
-                callbackUrl: "/client",
+                redirect: false,
             });
+            console.log(res);
+            if (res?.ok) {
+                router.push("/client");
+            } else {
+                throw new Error();
+            }
         } catch (error) {
-            alert("User already exists!");
+            setError("Wrong username and/or password!");
+            setLoading(false);
         }
+        setTimeout(() => {
+            setError("");
+        }, 8000);
+        setLoading(false);
         reset();
     };
 
     function logIn(provider: LiteralUnion<BuiltInProviderType>) {
+        setLoading(true);
         signIn(provider, {
             callbackUrl: "/client",
         });
@@ -44,7 +63,12 @@ export default function SigninForm() {
                 ... or <span className="font-extrabold">login</span> if you're a
                 client.
             </h2>
-            <p className="text-sm text-red-500">{errors.username?.message}</p>
+            {errors && (
+                <p className="text-sm text-red-500">
+                    {errors.username?.message}
+                </p>
+            )}
+            {error && <p className="text-sm text-red-500">{error}</p>}
             <input
                 type="text"
                 placeholder="Username"
@@ -67,13 +91,15 @@ export default function SigninForm() {
                 <button
                     className="flex items-center gap-3 cursor-pointer text-white bg-gradient-to-r from-gray-800 to-black px-3 py-3 rounded-2xl border border-gray-600 text-sm"
                     onClick={() => logIn("github")}
+                    type="button"
                 >
                     <FaGithub size={24} />
                     Login with GitHub
                 </button>
                 <button
-                    className="px-3 py-3 border flex items-center gap-3 dark:border-slate-700 rounded-2xl border-slate-400 text-slate-900 dark:text-slate-300 hover:shadow transition duration-150"
+                    className="px-3 py-3 border flex items-center gap-3 dark:border-slate-700 rounded-2xl border-slate-400 text-black dark:text-slate-300 hover:shadow transition duration-150"
                     onClick={() => logIn("google")}
+                    type="button"
                 >
                     <img
                         className="w-6 h-6"
@@ -85,10 +111,34 @@ export default function SigninForm() {
                 </button>
             </div>
             <button
+                disabled={loading}
                 type="submit"
-                className="rounded-xl p-2 w-1/2 bg-blue-600 text-white shadow-lg shadow-neutral-400"
+                className="rounded-xl p-2 w-1/2 bg-blue-600 text-white shadow-lg shadow-neutral-400 flex items-center justify-center"
             >
-                Login
+                {loading ? (
+                    <>
+                        <svg
+                            aria-hidden="true"
+                            role="status"
+                            className="inline w-4 h-4 me-3 text-white animate-spin"
+                            viewBox="0 0 100 101"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                fill="#E5E7EB"
+                            />
+                            <path
+                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                fill="currentColor"
+                            />
+                        </svg>
+                        Loading ...
+                    </>
+                ) : (
+                    <p>Login</p>
+                )}
             </button>
         </form>
     );
